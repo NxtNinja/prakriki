aeefff4e1172682b15688bc2ab91f13617b562e3
NxtNinja <136341993+NxtNinja@users.noreply.github.com>
2024-11-01 13:50:26 +0530

    favicon added and basic seo done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Roboto_Mono } from "next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Head from "next/head";
 
 const robotomono = Roboto_Mono({
   subsets: ["latin"],
@@ -18,10 +19,26 @@ const queryClient = new QueryClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <main className={`${robotomono.className}`}>
-        <Component {...pageProps} />
-      </main>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>Prakriti</title>
+        <link rel="icon" href="/favicon.ico" />
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Prakriti" />
+        <meta
+          property="og:description"
+          content="Get accurate and real-time weather forecasts and updates."
+        />
+        <meta property="og:image" content="/favicon.ico" />{" "}
+        {/* Replace with your app's image URL */}
+        <meta property="og:url" content="https://prakriti.netlify.app" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <main className={`${robotomono.className}`}>
+          <Component {...pageProps} />
+        </main>
+      </QueryClientProvider>
+    </>
   );
 }
